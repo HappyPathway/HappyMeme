7f3218254679843a9b5e78d3940edc93e09f7e66
Dave Arnold <darnold@l-sfh3k1htdd-m.gapinc.gap.com>
2018-05-08 09:59:51 -0700

    updating

--- build/playbooks/app_bootstrap.sh
+++ build/playbooks/app_bootstrap.sh
@@ -1,5 +1,5 @@
 sudo apt-get update
 sudo apt-get install -y python-pip
 sudo pip install -r /tmp/playbooks/requirements.txt
-sudo ansible-playbook /tmp/playbooks/app_install.yaml -e color=$(cat /tmp/color)
+sudo ansible-playbook /tmp/playbooks/app_install.yaml
 sudo rm -rf /tmp/playbooks
--- test/proxy_bootstrap.sh
+++
@@ -1,5 +0,0 @@
-sudo apt-get update
-sudo apt-get install -y python-pip
-sudo pip install -r /tmp/playbooks/requirements.txt
-sudo ansible-playbook /tmp/playbooks/proxy_install.yaml -e color=$(cat /tmp/color)
-sudo rm -rf /tmp/playbooks
